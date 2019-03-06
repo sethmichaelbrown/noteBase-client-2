8fca5a856b8b497941369c331a994b3ae23bd36a
SMB <sethmichaelbrown@gmail.com>
2019-03-05 17:26:52 -0700

    First commit - basic framework + deps

--- src/App.js
+++ src/App.js
@@ -2,27 +2,74 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Amplify, { Storage, API, graphqlOperation } from 'aws-amplify'
+import { withAuthenticator, S3Album } from 'aws-amplify-react'
+
+const listNotes = `query listNotes {
+  listNotes{
+    items{
+      id
+      name
+      codeNote
+      textNote
+    }
+  }
+}`
+
+const addNote = `mutation createNote($name:String! $codeNote:String $textNote:String) {
+  createNote(input:{
+    name:$name
+    codeNote:$codeNote
+    textNote:$textNote
+  }){
+    id
+    name
+    codeNote
+    textNote
+  }
+}`
+
 class App extends Component {
+
+  todoMutation = async () => {
+    const todoDetails = {
+      name: 'Party tonight!',
+      codeNote: "console.log('Hello World!')",
+      textNote: "How to log 'Hello World!'"
+    };
+    console.log('todoDetails', todoDetails)
+    const newEvent = await API.graphql(graphqlOperation(addNote, todoDetails));
+    console.log(JSON.stringify(newEvent));
+  }
+
+  listQuery = async () => {
+    console.log('listing todos');
+    const allTodos = await API.graphql(graphqlOperation(listNotes));
+    console.log(JSON.stringify(allTodos));
+  }
+
+  uploadFile = (evt) => {
+    const file = evt.target.files[0];
+    const name = file.name;
+
+    Storage.put(name, file).then(() => {
+      this.setState({ file: name });
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {/* Move to user settings page */}
+        <p> Pick a file</p>
+        <input type="file" onChange={this.uploadFile} />
+        <button onClick={this.listQuery}>GraphQL Query</button>
+        <button onClick={this.todoMutation}>GraphQL Mutation</button>
+        <S3Album level="private" path='' />
+        {/* End Move to user settings page */}
       </div>
     );
   }
 }
 
-export default App;
+export default withAuthenticator(App);
--- src/index.js
+++ src/index.js
@@ -4,6 +4,13 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import Amplify, { Storage, API, graphqlOperation } from 'aws-amplify'
+import config from './aws-exports'
+Amplify.configure(config)
+Storage.configure({ level: 'private' })
+
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
